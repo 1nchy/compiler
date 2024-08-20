a979d60271ad4d57b89f555ae714298e1f8bbcc3
1nchy <wangyinqi23@mails.ucas.ac.cn>
2024-08-20 17:09:59 +0800

    initialization

--- include/syntax/analyzer.hpp
+++ include/syntax/analyzer.hpp
@@ -1,6 +1,29 @@
 #ifndef _ICY_SYNTAX_ANALYZER_HPP_
 #define _ICY_SYNTAX_ANALYZER_HPP_
 
+#include <vector>
 
+#include "lexical/lexeme.hpp"
+#include "lexical/analyzer.hpp"
+
+namespace icy {
+
+namespace syntax {
+
+class analyzer;
+
+class analyzer {
+public:
+    analyzer() = default;
+    analyzer(const analyzer&) = delete;
+    analyzer& operator=(const analyzer&) = delete;
+    ~analyzer() = default;
+public:
+    void scan(const std::vector<lexical::lexeme>&);
+};
+
+}
+
+}
 
 #endif // _ICY_SYNTAX_ANALYZER_HPP_
--- main.cpp
+++ main.cpp
@@ -2,9 +2,11 @@
 
 #include "signal_stack.hpp"
 
+#include "argument_parser.hpp"
+
 int main(int _argc, char* _argv[]) {
     icy::signal_stack _s;
-
+    icy::argument_parser _p;
     icy::lexical::analyzer _a;
     const std::string _line = "const x = 1.2 + (3 * 4)";
     _a.scan(_line.cbegin(), _line.cend());
